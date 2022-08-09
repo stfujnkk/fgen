3fe8c851eb653fbca4af52f55d135c4083c847e3
stfujnkk <3365310020@qq.com>
2022-08-09 23:40:17 +0800

    fix(pathMapper): Matching Priority

--- fgen/core/router.py
+++ fgen/core/router.py
@@ -20,33 +20,33 @@ class Router:
                     uri = os.path.relpath(fullname, start=base)
                     ctx = {}
                     outputs = pm.mapping(uri, ctx)
-                    if not outputs:
-                        continue
-                    for output in outputs:
-                        node = Node()
-                        node.root = base
-                        node.template_path = uri
-                        node.output_path = output
-                        # 上下文
-                        tpl = self.conf.environment.get_template(uri)
-                        ctx['template_base'] = base
-                        ctx['template_path'] = uri
-                        ctx['file_name'] = os.path.basename(''.join(output))
-                        ctx['file_path'] = output
-                        ctx['match_groups'] = ctx['*']
-                        node.data = tpl.render(**ctx)
-                        for conv in pm.converters:
-                            if conv(node):
-                                continue
-                        for conv in self.conf.converters:
-                            if conv(node):
-                                continue
-                        # 默认输出到文件
-                        pth = node.output_path
-                        if isinstance(pth, list):
-                            pth = ''.join(pth)
-                        with open_for_save(os.path.join(self.conf.dest, pth)) as f:
-                            f.write(node.data)
+                    if outputs:
+                        for output in outputs:
+                            node = Node()
+                            node.root = base
+                            node.template_path = uri
+                            node.output_path = output
+                            # 上下文
+                            tpl = self.conf.environment.get_template(uri)
+                            ctx['template_base'] = base
+                            ctx['template_path'] = uri
+                            ctx['file_name'] = os.path.basename(''.join(output))
+                            ctx['file_path'] = output
+                            ctx['match_groups'] = ctx['*']
+                            node.data = tpl.render(**ctx)
+                            for conv in pm.converters:
+                                if conv(node):
+                                    continue
+                            for conv in self.conf.converters:
+                                if conv(node):
+                                    continue
+                            # 默认输出到文件
+                            pth = node.output_path
+                            if isinstance(pth, list):
+                                pth = ''.join(pth)
+                            with open_for_save(os.path.join(self.conf.dest, pth)) as f:
+                                f.write(node.data)
+                        break
                     pass
                 pass
         pass
