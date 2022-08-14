15ca9314cc47b95d8a4df22c4bd90bce0c4e974e
stfujnkk <3365310020@qq.com>
2022-08-14 10:34:07 +0800

    chore: venv and requirements

---
+++ install.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+python3 -m venv .env
+source .env/bin/activate.fish
+pip install -r requirements.txt
---
+++ shell.fish
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+fish -C "source .env/bin/activate.fish"
